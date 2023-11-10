3ba0cfa8d03a5503dea00b5b03c7cbffcb58cf41
Saimum-14028 <md.amiralfahim@gmail.com>
2023-11-10 08:32:16 +0600

    Fake data created alhamdulillah

--- src/HomeCards.jsx
+++ src/HomeCards.jsx
@@ -1,8 +1,30 @@
 const HomeCards = () => {
     return (
-        <div className="w-11/12">
-            <h1>Card Section</h1>
+        
+        <div>
+            <div className="w-11/12 bg-blue-200">
+                <h1 className="text-blue-800 text-sm font-normal bg-blue-400 w-20">Color</h1>
+                <h1 className="text-blue-800 text-xl font-semibold">Card Section</h1>
+            </div>
+
+            <div className="w-11/12 bg-red-200">
+                <h1 className="text-red-800 text-sm font-normal bg-red-400 w-20">Color</h1>
+                <h1 className="text-red-800 text-xl font-semibold">Card Section</h1>
+            </div>
+
+            <div className="w-11/12 bg-orange-200">
+                <h1 className="text-orange-800 text-sm font-normal bg-orange-400 w-20">Color</h1>
+                <h1 className="text-orange-800 text-xl font-semibold">Card Section</h1>
+            </div>
+
+            <div className="w-11/12 bg-green-200">
+                <h1 className="text-green-800 text-sm font-normal bg-green-400 w-20">Color</h1>
+                <h1 className="text-green-800 text-xl font-semibold">Card Section</h1>
+            </div>
+
         </div>
+
+        
     );
 };
 
