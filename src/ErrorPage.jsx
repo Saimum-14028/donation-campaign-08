55b568d9deac349b1cec3e469e9a5acf13fb2a23
Saimum-14028 <md.amiralfahim@gmail.com>
2023-11-10 20:51:29 +0600

    page not found completed alhamdulillah

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -1,6 +1,6 @@
 const ErrorPage = () => {
     return (
-        <div>
+        <div className="h-full flex justify-center items-center text-5xl font-bold text-red-700 mt-20">
             <h1>404 Not Found</h1>
         </div>
     );
